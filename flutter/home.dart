6ff9acede414aaee264838d12c1703ccb6cb6711
DavidInojosa <49165221+DavidInojosa@users.noreply.github.com>
2019-10-23 17:00:27 -0300

    Update home.dart

--- flutter/home.dart
+++ flutter/home.dart
@@ -1,50 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:aulas/core.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Home extends StatefulWidget {
+
+class CustomerList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  CustomerListState createState() => CustomerListState();
 }
 
-class BottomNavigator extends StatelessWidget {
-  final int _currentIndex;
-  BottomNavigator([this._currentIndex]);
+class CustomerListState extends State<CustomerList> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              title: Text("Treinamento")
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.contacts),
-              title: Text("Clientes")
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              title: Text("Mapeamento")
-          ),
-        ],
-
-      onTap: (index) {
-        if (index == 2)
-          Navigator.pushNamed(context, '/mapa');
-        if (index == 0)
-          Navigator.pushNamed(context, '/home');
-      },
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Clientes")
+      ),
+      bottomNavigationBar: BottomNavigator(1),
+      body: FutureBuilder(
+          future: http.get("${Server.serverUrl}clientes"),
+          builder: (context, snapshot) {
+            var customerList;
+            if (snapshot.hasData) {
+              customerList = json.decode(snapshot.data.body);
+              return ListView.separated(
+                  separatorBuilder: (context, i) => const Divider(),
+                  itemCount: customerList.length,
+                  itemBuilder: (context, i) {
+                    var clientName = customerList[i];
+                    return ListTile(
+                        title: Text('$clientName',
+                            style: Theme.of(context).textTheme.subhead),
+                        onTap: () =>
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>
+                                    CustomerView(i)
+                                ))
+                    );
+                  }
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    CustomerAdd()
+            )
+        )
+      ),
     );
   }
 }
 
-class _HomeState extends State<Home> {
+class CustomerAdd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Página inicial'),
+          title: Text("Adicionar Cliente"),
       ),
-      bottomNavigationBar: BottomNavigator(1),
+      body: Center(
+          child: Text("Hello World!")
+      )
       );
-    }
   }
+}
+
+class CustomerView extends StatefulWidget {
+  final int subjectId;
+
+  CustomerView(this.subjectId);
+
+  @override
+  CustomerViewState createState() => CustomerViewState();
+}
+
+
+class CustomerViewState extends State<CustomerView> {
+
+  Future< Map<String, dynamic> > _getCustomerData() async {
+    var future = await http.get("${Server.serverUrl}clientes/${widget.subjectId}");
+    return jsonDecode(future.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _customerData = _getCustomerData();
+    return Scaffold(
+      appBar: AppBar(
+        title: FutureBuilder(
+          future: _customerData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(
+                snapshot.data["nome"],
+                style: TextStyle(fontSize: 18.0),
+              );
+            }
+            return Text(
+              "Loading...",
+              style: TextStyle(fontSize: 18.0),
+            );
+          }
+        )
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FutureBuilder(
+                future: _customerData,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data["nome"]+" "+snapshot.data["sobrenome"]);
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }
+            ),
+            Text(
+            'id: ${widget.subjectId}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic)
+            ),
+            FutureBuilder(
+                future: _customerData,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data["telefone"]);
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }
+            )
+          ],
+        )
+    );
+  }
+}
