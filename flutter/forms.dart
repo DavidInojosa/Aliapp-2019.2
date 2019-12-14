fea4fce29330e2ecaf63c41c51f614e69c02fbb4
DavidInojosa <49165221+DavidInojosa@users.noreply.github.com>
2019-12-14 00:18:34 -0300

    Update forms.dart

--- flutter/forms.dart
+++ flutter/forms.dart
@@ -1,46 +1,79 @@
 import 'package:flutter/material.dart';
+import 'maps.dart';
 import 'core.dart';
 
-class PlaceholderDia extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
+  Widget build(BuildContext context) {
+    final appTitle = 'Adicionar Endereço/CEP';
+
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         appBar: AppBar(
-            title: Text("Ali App"),
-            actions: <Widget>[popupMenu(context)],
+          title: Text(appTitle),
+        
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.close),
+            onPressed: ()=> Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Mapa()
+            ),
+            ),
           ),
-        body: Center(child: Text("Oie galera")),
-        bottomNavigationBar: BottomNavigator(1),
-      );
-  Widget popupMenu(BuildContext context) => PopupMenuButton(
-      itemBuilder: (BuildContext context) => [
-        PopupMenuItem(
-          value: "server",
-          child: Text("Alterar servidor"),
+        ],
         ),
-        PopupMenuItem(
-          value: "logout",
-          child: Text("Trocar usuário")
-        ),
-      ],
-      onSelected: (result) {
-        switch (result) {
-          case "server":
-            Navigator.pushNamed(context, '/server');
-            break;
-          case "logout":
-            showDialog(
-                context: context,
-                builder: (context) => SimpleDialog(
-                    children: <Widget>[
-                      Center(child: Text("Função ainda não implementada! :(")),
-                      SimpleDialogOption(
-                          child: Text("OK", textAlign: TextAlign.right,),
-                          onPressed: () => Navigator.pop(context)),
-                    ],
-            ));
-            break;
-        }
-      }
-  );
+        bottomNavigationBar: BottomNavigator(Pages.Mapa),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                // se o forms estiver vazio
+                return 'Escreva algo aqui';
+              }
+            else{
+            print(value);
+            return null;
+            }
+              },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  // Se o form tiver algo inserido vai retornar :
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Adicionar'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
